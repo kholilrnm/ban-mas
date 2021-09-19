e6ae6b73053b12fa3f682b7a3999e36877205ecb
kholilboy <kholil.zboy@gmail.com>
2021-09-20 03:25:04 +0700

    Update 😅

--- app/Http/Controllers/PenerimaController.php
+++ app/Http/Controllers/PenerimaController.php
@@ -4,41 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\PemberianBantuan;
-use App\Warga;
 use DB;
 
 class PenerimaController extends Controller
 {
     public function penerima()
     {
-        // $data = PemberianBantuan::All();
-
         $data = DB::table('pemberian_bantuan')
-                ->leftJoin('warga', 'pemberian_bantuan.nik_warga', '=', 'warga.nik_warga')->get();
+                ->leftJoin('warga', 'pemberian_bantuan.nik_warga', '=', 'warga.nik_warga')
+                ->select('warga.nokk_warga', 'warga.nama_warga')
+                ->groupBy('warga.nik_warga', 'warga.nama_warga')
+                ->get();
 
-        // DB::table('users')
-        // ->leftJoin('posts', 'users.id', '=', 'posts.user_id')
-        // ->get();
-
-        // dd($data);
         return view('dashboard.penerima', compact("data"));
     }
 
-    public function show()
-    {
-        $dataPemberianBantuan = PemberianBantuan::All();
-        // dd($dataPemberianBantuan);
-        return datatables()->of($dataPemberianBantuan)
-            ->addIndexColumn()
-            ->addColumn('action', function ($row) {
-                
-            $data = 
-                '
-                <button>a</button>
-               ';
-            return $data;
-            })
-        ->rawColumns(['action'])
-        ->make(true);
-    }
 }
